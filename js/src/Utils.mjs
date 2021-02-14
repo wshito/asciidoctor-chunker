2dc3c966023160a62786f0d83a297e583a8d552a
Wataru Shito <wataru007jp@gmail.com>
2021-02-14 18:24:52 +0900

    added pipe()

---
+++ js/src/Utils.mjs
@@ -0,0 +1,12 @@
+/*
+ * This file is a part of Asciidoctor Chunker project.
+ * Copyright (c) 2021 Wataru Shito (@waterloo_jp)
+ */
+'use strict';
+
+export const pipe = (...funcs) => (...args) => {
+  let newArgs = args;
+  for (let i = 0; i < funcs.length; i++)
+    newArgs = [funcs[i].apply(this, newArgs)];
+  return newArgs[0];
+}
