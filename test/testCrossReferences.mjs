68641e3f6893883ef8d1b4d3398cdb9451de6971
Wataru Shito <wataru007jp@gmail.com>
2022-09-14 01:28:20 +0900

    removed Cheerio dependencies

--- test/testCrossReferences.mjs
+++ test/testCrossReferences.mjs
@@ -6,35 +6,35 @@
 'use strict';
 
 import test from 'ava';
-import { newDOM } from '../src/DOM.mjs';
+import Node from '../src/Node.mjs';
 
 test('test non-exisitent target link', t => {
   // Even if the target of the cross reference does not exist
   // the link with the anchor must be properly set.
-  const $single = newDOM('test/resources/output/single/sample.html');
-  const $chunked = newDOM('test/resources/output/html_chunks/chap3_sec3.html');
+  const $single = Node.getInstanceFromFile('test/resources/output/single/sample.html', null, true);
+  const $chunked = Node.getInstanceFromFile('test/resources/output/html_chunks/chap3_sec3.html', null, true);
   const selector = '#_chap3_third_section + div.paragraph p a';
-  const hrefSingle = $single(selector);
-  const hrefChunked = $chunked(selector);
+  const hrefSingle = $single.find(selector);
+  const hrefChunked = $chunked.find(selector);
   // test href attribute
-  t.is(hrefSingle.attr('href'), hrefChunked.attr('href'));
-  t.is('#chap4', hrefChunked.attr('href'));
+  t.is(hrefSingle.getAttr('href'), hrefChunked.getAttr('href'));
+  t.is('#chap4', hrefChunked.getAttr('href'));
   // test the link text
   t.is(hrefSingle.text(), hrefChunked.text());
   t.is('Chapter 4', hrefChunked.text());
   // test target-missing class is added to the chunked html
-  t.is('target-missing', hrefChunked.attr('class'));
+  t.is('target-missing', hrefChunked.getAttr('class'));
 });
 
 test('legal cross reference', t => {
-  const $single = newDOM('test/resources/output/single/sample.html');
-  const $chunked = newDOM('test/resources/output/html_chunks/chap2_sec3.html');
+  const $single = Node.getInstanceFromFile('test/resources/output/single/sample.html', null, true);
+  const $chunked = Node.getInstanceFromFile('test/resources/output/html_chunks/chap2_sec3.html', null, true);
   const selector = '#_chap2_third_section + div.paragraph + div.paragraph p a';
-  const hrefSingle = $single(selector);
-  const hrefChunked = $chunked(selector);
+  const hrefSingle = $single.find(selector);
+  const hrefChunked = $chunked.find(selector);
   // test href attribute
-  t.is('#chap3', hrefSingle.attr('href'));
-  t.is('chap3.html', hrefChunked.attr('href'));
+  t.is('#chap3', hrefSingle.getAttr('href'));
+  t.is('chap3.html', hrefChunked.getAttr('href'));
   // test the link text
   t.is(hrefSingle.text(), hrefChunked.text());
   t.is('Chapter 3', hrefChunked.text());
